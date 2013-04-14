bf2b3bd6f0c6d723f36a2da611cb5ab21ba83a50
owain lewis <owain@owainlewis.com>
2013-04-14 20:19:28 +0100

    adds test dir

--- project/Build.scala
+++ project/Build.scala
@@ -8,11 +8,11 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      // Add your project dependencies here,
+      "org.scalatest" % "scalatest_2.10.0-M7" % "2.0.M4-2.10.0-M7-B1" % "test"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
-      // Add your own project settings here      
+      // Add your own project settings here
     )
 
 }
---
+++ test/AppTest.scala
@@ -0,0 +1,19 @@
+import org.specs2.mutable._
+
+import play.api.test._
+import play.api.test.Helpers._
+
+class HelloWorldSpec extends Specification {
+
+  "The 'Hello world' string" should {
+    "contain 11 characters" in {
+      "Hello world" must have size(11)
+    }
+    "start with 'Hello'" in {
+      "Hello world" must startWith("Hello")
+    }
+    "end with 'world'" in {
+      "Hello world" must endWith("world")
+    }
+  }
+}
